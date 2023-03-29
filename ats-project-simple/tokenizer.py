ab9d63d9355cf6e51620d81bd686fb29b6d2015d
patrycjarecko <patrycjarecko00@gmail.com>
2023-03-29 18:02:52 +0200

    fix: pop first value from tokens list instead of comparing values, go back to previous function name

--- ats-project-simple/tokenizer.py
+++ ats-project-simple/tokenizer.py
@@ -1,12 +1,13 @@
-import tokenize
+import tokenize as tk
 from io import BytesIO
 
 
-def tokenize_code(text: str):
-    tokens = list(tokenize.tokenize(BytesIO(text.encode("utf-8")).readline))
+def tokenize(text: str):
+    tokens = list(tk.tokenize(BytesIO(text.encode("utf-8")).readline))
     tokens = [
         token.string.strip()
         for token in tokens
-        if token.type != tokenize.NEWLINE and token.string.strip() not in ["utf-8", ""]
+        if token.type != tk.NEWLINE and token.string.strip() != ""
     ]
+    tokens.pop(0)
     return tokens
