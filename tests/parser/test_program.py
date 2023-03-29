c68534e948e28b16fae7dd9992b7e4609e822638
wvffle <github@wvffle.net>
2023-03-29 21:50:49 +0000

    test: split up the test files

--- tests/parser/test_program.py
+++ tests/parser/test_program.py
@@ -4,5 +4,5 @@ from ats_project_simple.parser import parse
 
 
 def test_empty_program():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, match="Expected token 'procedure', got end of file"):
         parse("")
