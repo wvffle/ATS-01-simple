724bd8755793af76ef6f713cfe0664d827121398
wvffle <github@wvffle.net>
2023-03-29 21:50:49 +0000

    test: split up the test files

---
+++ tests/parser/test_procedure.py
@@ -0,0 +1,13 @@
+import pytest
+
+from ats_project_simple.parser import parse
+
+
+def test_empty_procedure_no_name():
+    with pytest.raises(ValueError, match="Token '{' is not a valid NAME_TOKEN"):
+        parse("procedure {}")
+
+
+def test_empty_procedure_no_brackets():
+    with pytest.raises(ValueError, match="Expected token '{', got end of file"):
+        parse("procedure proc")
