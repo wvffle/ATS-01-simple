1e12d18e523bf20f64db463f21f9e33d6ad63c60
Kamil Szczerbak <kamilszczerbak43@gmail.com>
2023-04-03 16:47:05 +0000

    feat: follows

--- ats/pkb/pkb.py
+++ ats/pkb/pkb.py
@@ -1,33 +1,27 @@
-def follows(list, s1, s2):
+map = {"s1"}
+
+
+def add(list):
     all_children = set(list.children)
     pool = all_children.copy()
+    i = 0
     while pool:
         child = pool.pop()
         all_children.add(child)
         pool.update(child.children)
-        for i, ch in enumerate(child.children):
-            if s1 in child.children.__str__() and s2 in child.children.__str__():
+        if "stmt:" in child.str():
+            map["s1"].append(child)
+            i = i + 1
+
+
+def follows(s1: str, s2: str):
+    i = 0
+    while map:
+        # print(map[2])
+        if i + 1 < len(map):
+            if s1 in map[i].str() and s2 in map[i + 1].str():
+                print(i)
                 return True
             else:
                 return False
-
-
-def parent(list, s1, s2):
-    all_children = set(list.children)
-    pool = all_children.copy()
-    while pool:
-        child = pool.pop()
-        all_children.add(child)
-        pool.update(child.children)
-        for i, ch in enumerate(child.children):
-            if s1 in ch.__str__():
-                if "stmtLst" in ch.children.__str__():
-                    for x in ch.children:
-                        if s2 in x.children.__str__():
-                            return True
-                        else:
-                            return False
-                else:
-                    return False
-            else:
-                return False
+        i = i + 1
