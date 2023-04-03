b315dc273dd3d86299433e320b95e7d4a7a21f54
Kasper Seweryn <github@wvffle.net>
2023-04-03 19:21:27 +0200

    feat: set global output encoding to utf-8

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+
 from ats.parser.parser import parse
 
 if __name__ == "__main__":
+    sys.stdout.reconfigure(encoding="utf-8")
+
     tree = parse(
         """
         procedure test {
