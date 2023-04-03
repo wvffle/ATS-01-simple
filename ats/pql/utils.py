7b092d04c1a03953b3f66410f3fbe4661ca03e99
SiwyKot41 <65611113+SiwyKot41@users.noreply.github.com>
2023-04-03 14:19:51 +0200

    refactor(pql): simplify return statement in is_program_design_entity_relationship_token()
    Co-authored-by: Kasper Seweryn <github@wvffle.net>

--- ats/pql/utils.py
+++ ats/pql/utils.py
@@ -5,7 +5,7 @@ def is_variable_type_token(text: str):
     return True if text == "stmt" or text == "assign" or text == "while" else False
 
 def is_program_design_entity_relationship_token(text: str):
-    return True if text == "Modifies" or text == "Follows" or text == "Parent" else False
+    return text in ["Modifies", "Follows", "Parent"]
 
 def is_string_token(text: str):
     return bool(re.match(r'^"[^"]*"$', text))
