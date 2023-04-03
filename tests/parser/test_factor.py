9942403388b2cca88d155bb43078c69994b7ab3e
patrycjarecko <patrycjarecko00@gmail.com>
2023-04-03 17:52:09 +0200

    refactor: rename test for checking restricted keywords in plus expression for factor

--- tests/parser/test_factor.py
+++ tests/parser/test_factor.py
@@ -9,7 +9,7 @@ def test_factor_restricted_keyword_as_var():
             parse(f"procedure proc {{ a = {x}; }}")
 
 
-def test_factor_restricted_keyword_as_var_with_adding():
+def test_factor_restricted_keyword_as_var_in_plus_expr():
     for x in ["if", "then", "else", "while", "procedure"]:
         with pytest.raises(ValueError, match=f"Token '{x}' is a reserved keyword"):
             parse(f"procedure proc {{ a = {x} + 8; }}")
