9e395e665cfa26fd6f41eb3cd63c0713c0a18671
Kasper Seweryn <github@wvffle.net>
2023-04-03 19:29:12 +0200

    feat(cli): add --print-test-tree flag

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ from ats.parser.parser import parse
 if __name__ == "__main__":
     sys.stdout.reconfigure(encoding="utf-8")
 
-    tree = parse(
-        """
-        procedure test {
-            a = 8;
-            while a {
-                a = a + 1;
+    if "--print-test-tree" in sys.argv:
+        tree = parse(
+            """
+            procedure test {
+                a = 8;
+                while a {
+                    a = a + 1;
+                }
+                if b then {
+                    a = a + 2;
+                }
+                else {
+                    a = a + 3;
+                }
             }
-            if b then {
-                a = a + 2;
-            }
-            else {
-                a = a + 3;
-            }
-        }
-        """
-    )
+            """
+        )
 
-    tree.print_tree()
+        tree.print_tree()
