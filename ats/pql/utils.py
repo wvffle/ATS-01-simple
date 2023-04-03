0d6ab22f07e3023df156e25daa1385381a86d309
SiwyKot41 <65611113+SiwyKot41@users.noreply.github.com>
2023-04-03 14:15:50 +0200

    fix(pql): fix string matching
    Co-authored-by: Kasper Seweryn <github@wvffle.net>

--- ats/pql/utils.py
+++ ats/pql/utils.py
@@ -8,4 +8,4 @@ def is_program_design_entity_relationship_token(text: str):
     return True if text == "Modifies" or text == "Follows" or text == "Parent" else False
 
 def is_string_token(text: str):
-    return bool(re.match(r'^".*"$', text))
+    return bool(re.match(r'^"[^"]*"$', text))
