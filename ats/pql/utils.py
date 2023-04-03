d741adb040972a09653ce233837b5aacbe4a820d
SiwyKot41 <frutkowski80@gmail.com>
2023-04-03 13:33:56 +0000

    refactor(pql): fix linting

--- ats/pql/utils.py
+++ ats/pql/utils.py
@@ -4,8 +4,10 @@ import re
 def is_variable_type_token(text: str):
     return text in ["stmt", "assign", "while"]
 
+
 def is_program_design_entity_relationship_token(text: str):
     return text in ["Modifies", "Follows", "Parent"]
+
 
 def is_string_token(text: str):
     return bool(re.match(r'^"[^"]*"$', text))
