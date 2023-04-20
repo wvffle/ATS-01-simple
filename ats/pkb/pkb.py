c2116cf66f77858506bd0e1132c6244454250297
Michal_Parchanowicz <michal.parchanowicz@gmail.com>
2023-04-20 20:07:55 +0200

    feat: create follows dictionary

--- ats/pkb/pkb.py
+++ ats/pkb/pkb.py
@@ -1,5 +1,7 @@
 from ats.ast import nodes
 
+follow_dictionary = {}
+
 
 def process_follows(query, statements):
     import json
@@ -9,11 +11,45 @@ def process_follows(query, statements):
     return []
 
 
-def evaluate_query(tree: nodes.ProgramNode, query):
+def crecreate_follow_dictionary(tree: nodes.ProgramNode):
+    parent_childrens_list = []
+
+    def create_parent_childrens_list(node: nodes.ASTNode):
+        childrens = []
+        for i in range(len(node.children)):
+            create_parent_childrens_list(node.children[i])
+            childrens.append(node.children[i])
+        if len(node.children) > 0:
+            parent_childrens_list.insert(0, childrens)
+
+    create_parent_childrens_list(tree)
+
+    for i in range(len(parent_childrens_list)):
+        if len(parent_childrens_list[i]) > 1:
+            for j in range(len(parent_childrens_list[i]) - 1):
+                if (
+                    str(parent_childrens_list[i][j]).split(": ")[0] == "stmt"
+                    and str(parent_childrens_list[i][j]).split(": ")[0]
+                    == str(parent_childrens_list[i][j + 1]).split(": ")[0]
+                ):
+                    follow_dictionary[str(parent_childrens_list[i][j])] = str(
+                        parent_childrens_list[i][j + 1]
+                    )
+
+
+def evaluate_query(query):
+    print(follow_dictionary)
+
+    print(query)
+    """
+    if query["relation"] == "Follows":
+        type = str(query["variables"][query["searching_variable"]])
+
+
+
+
     statements = {}
-
     i = 1
-
     def find_statements(node: nodes.ASTNode):
         if isinstance(node, nodes.StmtNode):
             nonlocal i
@@ -24,6 +60,7 @@ def evaluate_query(tree: nodes.ProgramNode, query):
             find_statements(n)
 
     find_statements(tree)
+    print(statements)
 
     if query["relation"] == "Follows":
-        return process_follows(query, statements)
+        return process_follows(query, statements)"""
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from ats.parser.parser import parse
-from ats.pkb.pkb import evaluate_query
+from ats.pkb.pkb import crecreate_follow_dictionary, evaluate_query
 from ats.pql.pql import parse_pql
 
 if __name__ == "__main__":
@@ -13,21 +13,23 @@ if __name__ == "__main__":
             procedure test {
                 a = 8;
                 while a {
-                if b then {
-                    a = a + 2;
-                }
-                else {
-                    a = a + 3;
-                }
+                    if b then {
+                        a = a + 2;
+                    }
+                    else {
+                        a = a + 3;
+                    }
                     a = a + 1;
                 }
+                b = 10;
             }
             """
         )
 
         tree.print_tree()
         queries = parse_pql("stmt s1, s2; Select s1 such that Follows(s1, s2)")
-        evaluate_query(tree, queries[0])
+        crecreate_follow_dictionary(tree)
+        evaluate_query(queries[0])
 
     elif len(sys.argv) > 0:
         with open(sys.argv[-1]) as f:
