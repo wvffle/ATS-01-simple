8323eb3836aa280cbac74e91f2e4cda545138325
Kasper Seweryn <github@wvffle.net>
2023-06-08 20:51:42 +0200

    refactor: simplify pkb code (#27)
    * refactor: simplify pkb code
    
    * refactor: remove unused code
    
    * refactor: simplify relations
    
    * refactor: simplify dfs algorithms

--- ats/pkb/pkb.py
+++ ats/pkb/pkb.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 from ats.ast import nodes
 
 STMT_TYPE_MAP = {
@@ -8,184 +10,139 @@ STMT_TYPE_MAP = {
 }
 
 
+def dfs(
+    node: nodes.ASTNode,
+    on_node_enter: Callable[[nodes.ASTNode], None] = lambda _: None,
+    on_node_exit: Callable[[nodes.ASTNode], None] = lambda _: None,
+):
+    on_node_enter(node)
+
+    for child in node.children:
+        dfs(child, on_node_enter, on_node_exit)
+
+    on_node_exit(node)
+
+
 def preprocess_query(tree: nodes.ProgramNode):
     statements = {}
     variables = {}
     follows = {}
-    parents = {}
 
-    def find_all_statements():
-        i = 1
+    def find_statements():
+        stmt_index = 0
+        stmt_id = 1
 
-        def find_statements_and_variables(node: nodes.ASTNode):
+        def on_node_enter(node: nodes.ASTNode):
+            nonlocal stmt_index
+            nonlocal stmt_id
+
+            # Reset the index when we enter a new statement list
+            if isinstance(node, nodes.StmtLstNode):
+                stmt_index = 0
+
+            # Assign an index and a unique id to each statement
             if isinstance(node, nodes.StmtNode):
-                nonlocal i
-                statements[i] = node
-                node.__stmt_id = i
-                i += 1
+                statements[stmt_id] = node
+                node.__stmt_id = stmt_id
+                node.__stmt_index = stmt_index
 
+                stmt_index += 1
+                stmt_id += 1
+
+            # Find all variables
             if isinstance(node, nodes.VariableNode):
                 variables[node] = node.name
 
-            for n in node.children:
-                find_statements_and_variables(n)
+        dfs(tree, on_node_enter=on_node_enter)
 
-        find_statements_and_variables(tree)
-
-    def process_all_relations():
-        def process_relations(node: nodes.ASTNode):
-            if isinstance(node, nodes.StmtLstNode):
-                for i, child in enumerate(node.children):
-                    if i > 0:
-                        follows[child.__stmt_id] = node.children[i - 1].__stmt_id
-
+    def process_relations():
+        def on_node_enter(node: nodes.ASTNode):
+            # Build the follows relation map
             if isinstance(node, nodes.StmtNode):
-                parent = node.parent.parent
-                if isinstance(parent, nodes.StmtNode):
-                    parents[node.__stmt_id] = parent.__stmt_id
+                if node.__stmt_index > 0:
+                    follows[node] = node.parent.children[node.__stmt_index - 1]
 
-            for child in node.children:
-                process_relations(child)
+        dfs(tree, on_node_enter=on_node_enter)
 
-        process_relations(tree)
-
-    find_all_statements()
-    process_all_relations()
+    find_statements()
+    process_relations()
 
     return {
         "statements": statements,
         "follows": follows,
-        "parents": parents,
     }
+
+
+def _get_node(query, context, statement, parameter):
+    if isinstance(parameter, int):
+        return context["statements"][parameter]
+    else:
+        if not isinstance(statement, STMT_TYPE_MAP[query["variables"][parameter]]):
+            raise KeyError("Invalid variable type")
+
+        return statement
+
+
+def _resolve_statements(query, a, b, statement, related_statement):
+    if query["searching_variable"] == a:
+        return related_statement(statement), statement, related_statement(statement)
+    if query["searching_variable"] == b:
+        return related_statement(statement), statement, statement
+    if not isinstance(a, int) and not isinstance(b, int):
+        return related_statement(statement), statement, related_statement(statement)
+    return statement, statement, statement
 
 
 def process_follows(query, context):
     a = query["relations"][0]["parameters"][0]
     b = query["relations"][0]["parameters"][1]
     follows = context["follows"]
-    statements = context["statements"]
 
-    result = []
-
+    result = set()
     for stmt in context["statements"].values():
         try:
-            # case 1 - constant and constant
-            if isinstance(a, int) and isinstance(b, int):
-                if follows[b] == a:
-                    result.append(stmt.__stmt_id)
 
-            # 2 - constant and variable
-            if isinstance(a, int) and not isinstance(b, int):
-                # Check the variable type
-                if not isinstance(stmt, STMT_TYPE_MAP[query["variables"][b]]):
-                    continue
+            def relation(statement):
+                return follows[statement]
 
-                # Check relation
-                if follows[stmt.__stmt_id] == a:
-                    result.append(stmt.__stmt_id)
+            stmt_a, stmt_b, searching = _resolve_statements(query, a, b, stmt, relation)
+            node_a = _get_node(query, context, stmt_a, a)
+            node_b = _get_node(query, context, stmt_b, b)
 
-            # case 3 - variable and constant
-            if not isinstance(a, int) and isinstance(b, int):
-                # Check the variable type
-                if not isinstance(stmt, STMT_TYPE_MAP[query["variables"][a]]):
-                    continue
-
-                # Check relation
-                if follows[b] == stmt.__stmt_id:
-                    result.append(stmt.__stmt_id)
-
-            #  case 4 - varibale and variable
-            if not isinstance(a, int) and not isinstance(b, int):
-                if query["searching_variable"] == a:
-                    s2 = stmt
-                    s1 = statements[follows[stmt.__stmt_id]]
-                elif query["searching_variable"] == b:
-                    s2 = statements[follows[stmt.__stmt_id]]
-                    s1 = stmt
-                else:
-                    s2 = stmt
-                    s1 = statements[follows[stmt.__stmt_id]]
-
-                # Check the variable a type
-                if not isinstance(s1, STMT_TYPE_MAP[query["variables"][a]]):
-                    continue
-
-                # Check the variable b type
-                if not isinstance(s2, STMT_TYPE_MAP[query["variables"][b]]):
-                    continue
-
-                # Check relation
-                result.append(s1.__stmt_id)
+            if relation(node_b) == node_a:
+                result.add(searching.__stmt_id)
 
         except KeyError:
             pass
-    return result
+
+    return list(result)
 
 
 def process_parent(query, context):
     a = query["relations"][0]["parameters"][0]
     b = query["relations"][0]["parameters"][1]
-    parent = context["parents"]
-    statements = context["statements"]
 
-    result = []
-
+    result = set()
     for stmt in context["statements"].values():
         try:
-            # case 1 - constant and constant
-            if isinstance(a, int) and isinstance(b, int):
-                if parent[b] == a:
-                    result.append(stmt.__stmt_id)
 
-            # 2 - constant and variable
-            if isinstance(a, int) and not isinstance(b, int):
-                # Check the variable type
-                if not isinstance(stmt, STMT_TYPE_MAP[query["variables"][b]]):
-                    continue
+            def relation(statement):
+                return statement.parent.parent
 
-                # Check relation
-                if parent[stmt.__stmt_id] == a:
-                    result.append(stmt.__stmt_id)
+            stmt_a, stmt_b, searching = _resolve_statements(query, a, b, stmt, relation)
+            node_a = _get_node(query, context, stmt_a, a)
+            node_b = _get_node(query, context, stmt_b, b)
 
-            # case 3 - variable and constant
-            if not isinstance(a, int) and isinstance(b, int):
-                # Check the variable type
-                if not isinstance(stmt, STMT_TYPE_MAP[query["variables"][a]]):
-                    continue
-
-                # Check relation
-                if parent[b] == stmt.__stmt_id:
-                    result.append(stmt.__stmt_id)
-
-            #  case 4 - varibale and variable
-            if not isinstance(a, int) and not isinstance(b, int):
-                if query["searching_variable"] == a:
-                    s2 = stmt
-                    s1 = statements[parent[stmt.__stmt_id]]
-                elif query["searching_variable"] == b:
-                    s2 = statements[parent[stmt.__stmt_id]]
-                    s1 = stmt
-                else:
-                    s2 = stmt
-                    s1 = statements[parent[stmt.__stmt_id]]
-
-                # Check the variable a type
-                if not isinstance(s1, STMT_TYPE_MAP[query["variables"][a]]):
-                    continue
-
-                # Check the variable b type
-                if not isinstance(s2, STMT_TYPE_MAP[query["variables"][b]]):
-                    continue
-
-                # Check relation
-                result.append(s1.__stmt_id)
+            if relation(node_b) == node_a:
+                result.add(searching.__stmt_id)
 
         except KeyError:
             pass
-    return result
+
+    return list(result)
 
 
-def evaluate_query(node: nodes.ASTNode, query):
+def evaluate_query(node: nodes.ProgramNode, query):
     context = preprocess_query(node)
     if query["relations"][0]["relation"] == "Follows":
         return process_follows(query, context)
