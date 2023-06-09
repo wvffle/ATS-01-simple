7b5df8d50591eba2247d3eddbe72fb3479b73fa3
Kasper Seweryn <github@wvffle.net>
2023-06-09 13:28:57 +0200

    fix(tokenizer): support  character

--- ats/tokenizer.py
+++ ats/tokenizer.py
@@ -3,11 +3,17 @@ from io import BytesIO
 
 
 def tokenize(text: str):
-    tokens = list(tk.tokenize(BytesIO(text.encode("utf-8")).readline))
-    tokens = [
-        token.string.strip()
-        for token in tokens
-        if token.type != tk.NEWLINE and token.string.strip() != ""
-    ]
-    tokens.pop(0)
+    tokens = []
+    for token in tk.tokenize(BytesIO(text.encode("utf-8")).readline):
+        if token.type == tk.ENCODING:
+            continue
+
+        if token.type == tk.COMMENT:
+            tokens.append("#")
+            tokens += tokenize(token.string[1:])
+            continue
+
+        if token.type != tk.NEWLINE and token.string.strip() != "":
+            tokens.append(token.string)
+
     return tokens
