56e323d237edb4e5b4de4de100cc1435f3b5de19
Kamil Szczerbak <kamilszczerbak43@gmail.com>
2023-06-09 14:30:56 +0000

    fix: rebase

--- tests/pkb/test_follows.py
+++ tests/pkb/test_follows.py
@@ -21,69 +21,94 @@ def _get_ast_tree():
     )
 
 
-def test_pkb_follows_const_const():
-    tree = _get_ast_tree()
+tree = _get_ast_tree()
 
+
+def test_pkb_follows_const_const():
     queries = parse_query("stmt s1; Select s1 such that Follows(1, 2)")
     result = evaluate_query(tree, queries[0])
     assert result == [1, 2, 3, 4, 5, 6, 7]
 
+
+def test_pkb_follows_const_const_1():
     queries = parse_query("stmt s1; Select s1 such that Follows(2, 1)")
     result = evaluate_query(tree, queries[0])
     assert result == []
 
 
 def test_pkb_follows_stmt_const():
-    tree = _get_ast_tree()
-
     queries = parse_query("stmt s1; Select s1 such that Follows(s1, 2)")
     result = evaluate_query(tree, queries[0])
     assert result == [1]
 
+
+def test_pkb_follows_stmt_const_1():
     queries = parse_query("assign a1; Select a1 such that Follows(a1, 2)")
     result = evaluate_query(tree, queries[0])
     assert result == [1]
 
+
+def test_pkb_follows_stmt_const_2():
     queries = parse_query("while w1; Select w1 such that Follows(w1, 2)")
     result = evaluate_query(tree, queries[0])
     assert result == []
 
 
 def test_pkb_follows_const_stmt():
-    tree = _get_ast_tree()
-
     queries = parse_query("stmt s1; Select s1 such that Follows(1, s1)")
     result = evaluate_query(tree, queries[0])
     assert result == [2]
 
+
+def test_pkb_follows_const_stmt_1():
     queries = parse_query("assign a1; Select a1 such that Follows(1, a1)")
     result = evaluate_query(tree, queries[0])
     assert result == [2]
 
+
+def test_pkb_follows_const_stmt_2():
     queries = parse_query("while w1; Select w1 such that Follows(1, w1)")
     result = evaluate_query(tree, queries[0])
     assert result == []
 
 
 def test_pkb_follows_stmt_stmt():
-    tree = _get_ast_tree()
-
     queries = parse_query("stmt s1, s2; Select s1 such that Follows(s1, s2)")
     result = evaluate_query(tree, queries[0])
     assert result == [1, 2, 3, 4, 6]
 
+
+def test_pkb_follows_stmt_stmt_1():
     queries = parse_query("assign a1, a2; Select a1 such that Follows(a1, a2)")
     result = evaluate_query(tree, queries[0])
     assert result == [1, 2, 3, 6]
 
+
+def test_pkb_follows_stmt_stmt_2():
     queries = parse_query("assign a1, a2; Select a2 such that Follows(a1, a2)")
     result = evaluate_query(tree, queries[0])
     assert result == [2, 3, 4, 7]
 
+
+def test_pkb_follows_stmt_stmt_3():
     queries = parse_query("stmt s1, s2; Select s2 such that Follows(s1, s2)")
     result = evaluate_query(tree, queries[0])
     assert result == [2, 3, 4, 5, 7]
 
+
+def test_pkb_follows_stmt_stmt_4():
     queries = parse_query("stmt s1, s2, s3; Select s3 such that Follows(s1, s2)")
     result = evaluate_query(tree, queries[0])
-    assert result == [1, 2, 3, 4, 5, 6, 7]
+    assert result == [1, 2, 3, 4, 6]
+
+
+def test_pkb_follows_stmt_stmt_5():
+    queries = parse_query("variable s1; stmt s2; Select s1 such that Follows(s1, s2)")
+    result = evaluate_query(tree, queries[0])
+    assert result == []
+
+
+def test_pkb_follows_stmt_stmt_6():
+    queries = parse_query("variable s2; stmt s1; Select s1 such that Follows(s1, s2)")
+    result = evaluate_query(tree, queries[0])
+    assert result == []
--- tests/pkb/test_modifies.py
+++ tests/pkb/test_modifies.py
@@ -39,66 +39,82 @@ def _get_ast_tree():
     )
 
 
+tree = _get_ast_tree()
+
+
 def test_pkb_modifies_const_variable():
-    tree = _get_ast_tree()
     queries = parse_query("""while w1; Select w1 such that Modifies(1, "a")""")
     result = evaluate_query(tree, queries[0])
     assert result == [1]
+
+
+def test_pkb_modifies_const_variable_1():
     queries = parse_query("""variable v1; Select v1 such that Modifies(1, v1)""")
     result = evaluate_query(tree, queries[0])
     assert result == ["a"]
 
+
+def test_pkb_modifies_const_variable_2():
     queries = parse_query("""while w1; Select w1 such that Modifies(2, "a")""")
     result = evaluate_query(tree, queries[0])
     assert result == [2]
+
+
+def test_pkb_modifies_const_variable_3():
     queries = parse_query("""variable v1; Select v1 such that Modifies(2, v1)""")
     result = evaluate_query(tree, queries[0])
     assert result == ["a", "b", "c"]
 
 
 def test_pkb_modifies_assign_variable():
-    tree = _get_ast_tree()
     queries = parse_query("""assign a1; Select a1 such that Modifies(a1, "a")""")
     result = evaluate_query(tree, queries[0])
     assert result == [1, 3, 11, 15]
 
+
+def test_pkb_modifies_assign_variable_1():
     queries = parse_query("""assign a1; Select a1 such that Modifies(a1, "c")""")
     result = evaluate_query(tree, queries[0])
     assert result == [6, 9]
 
 
 def test_pkb_modifies_while_variable():
-    tree = _get_ast_tree()
     queries = parse_query("""while w1; Select w1 such that Modifies(w1, "a")""")
     result = evaluate_query(tree, queries[0])
     assert result == [2, 12]
 
 
 def test_pkb_modifies_if_variable():
-    tree = _get_ast_tree()
     queries = parse_query("""if i1; Select i1 such that Modifies(i1, "c")""")
     result = evaluate_query(tree, queries[0])
     assert result == [4, 13]
+
+
+def test_pkb_modifies_if_variable_1():
     queries = parse_query("""if i1; Select i1 such that Modifies(i1, "f")""")
     result = evaluate_query(tree, queries[0])
     assert result == [13]
 
 
 def test_pkb_modifies_statements_variable():
-    tree = _get_ast_tree()
     queries = parse_query("""stmt s1; Select s1 such that Modifies(s1, "c")""")
     result = evaluate_query(tree, queries[0])
     assert result == [2, 4, 6, 8, 9, 12, 13, 14]
 
 
 def test_pkb_modifies_procedure_variable():
-    tree = _get_ast_tree()
     queries = parse_query("""procedure p1; Select p1 such that Modifies(p1, "a")""")
     result = evaluate_query(tree, queries[0])
     assert result == ["test", "test2"]
+
+
+def test_pkb_modifies_procedure_variable_1():
     queries = parse_query("""procedure p1; Select p1 such that Modifies(p1, "i")""")
     result = evaluate_query(tree, queries[0])
     assert result == ["test2"]
+
+
+def test_pkb_modifies_procedure_variable_2():
     queries = parse_query("""procedure p1; Select p1 such that Modifies(p1, "q")""")
     result = evaluate_query(tree, queries[0])
     assert result == []
@@ -142,21 +158,28 @@ def _get_ast_tree_call():
     )
 
 
+tree_call = _get_ast_tree_call()
+
+
 def test_pkb_modifies_call_procedure_variable():
-    tree = _get_ast_tree_call()
     queries = parse_query("""procedure p1; Select p1 such that Modifies(p1, "c")""")
-    result = evaluate_query(tree, queries[0])
+    result = evaluate_query(tree_call, queries[0])
     assert result == ["test1", "test2", "test3"]
+
+
+def test_pkb_modifies_call_procedure_variable_1():
     queries = parse_query("""procedure p1; Select p1 such that Modifies(p1, "e")""")
-    result = evaluate_query(tree, queries[0])
+    result = evaluate_query(tree_call, queries[0])
     assert result == ["test4", "test5", "test6"]
 
 
 def test_pkb_modifies_call_stmt_variable():
-    tree = _get_ast_tree_call()
     queries = parse_query("""stmt s1; Select s1 such that Modifies(s1, "c")""")
-    result = evaluate_query(tree, queries[0])
+    result = evaluate_query(tree_call, queries[0])
     assert result == [2, 3, 4, 5]
+
+
+def test_pkb_modifies_call_stmt_variable_1():
     queries = parse_query("""stmt s1; Select s1 such that Modifies(s1, "e")""")
-    result = evaluate_query(tree, queries[0])
+    result = evaluate_query(tree_call, queries[0])
     assert result == [6, 7, 8]
