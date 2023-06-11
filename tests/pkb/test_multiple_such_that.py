4e0fea818ff9dc59a9f6c3fdf66c25ecb2bfabd3
SiwyKot41 <frutkowski80@gmail.com>
2023-06-11 12:42:13 +0000

    fix(pkb): fix multiple such that queries

---
+++ tests/pkb/test_multiple_such_that.py
@@ -0,0 +1,37 @@
+from ats.parser.parser import parse
+from ats.pkb.pkb import evaluate_query
+from ats.pql.pql import parse_query
+
+
+def _get_ast_tree():
+    return parse(
+        """
+            procedure proc {
+                a = 1;
+                a = 1;
+                a = 1;
+                a = 1;
+
+                while a {
+                    a = 1;
+                    a = 1;
+                }
+            }
+            """
+    )
+
+
+tree = _get_ast_tree()
+
+
+def test_pkb_follows_complex():
+    queries = parse_query(
+        """
+        stmt s1;
+        while w;
+        assign a;
+        Select s1 such that Follows(s1, a) and Parent(w, s1)
+        """
+    )
+    result = evaluate_query(tree, queries[0])
+    assert result == [6]
